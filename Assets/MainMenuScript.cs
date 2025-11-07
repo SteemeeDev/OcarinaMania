4fa38a66f9396de3a552f6c9226ef24900149849
Kattemadsguden <144560726+Kattemadsguden@users.noreply.github.com>
2025-11-07 09:46:43 +0100

    Main menu completed

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -9,4 +9,10 @@ public class MainMenuScript : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }  
+
